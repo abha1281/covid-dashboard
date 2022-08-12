7f9ca2e451ade12752bb9d93e128ce1195413556
abha <a1b2h8a1@gmail.com>
2022-08-12 08:00:51 +0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "../styles/custom.scss";
+import type { AppProps } from "next/app";
+import MainTemplate from "../components/layout/template/MainTemplate";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MainTemplate>
+      <Component {...pageProps} />
+    </MainTemplate>
+  );
 }
 
-export default MyApp
+export default MyApp;
