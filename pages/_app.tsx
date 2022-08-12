ced65613e9a688c95aaf203eb0d176743dfecba0
abha <abhaaibrahim@gmail.com>
2022-08-12 20:21:42 +0500

    use recoil

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import "../styles/globals.css";
 import "../styles/custom.scss";
 import type { AppProps } from "next/app";
 import MainTemplate from "../components/layout/template/MainTemplate";
+import { RecoilRoot } from "recoil";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MainTemplate>
-      <Component {...pageProps} />
-    </MainTemplate>
+    <RecoilRoot>
+      <MainTemplate>
+        <Component {...pageProps} />
+      </MainTemplate>
+    </RecoilRoot>
   );
 }
 
