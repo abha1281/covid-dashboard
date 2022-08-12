19575aad50e5420e8761c96dc9043a4b673e8609
abha <abhaaibrahim@gmail.com>
2022-08-12 20:20:55 +0500

    Create Meta.tsx

---
+++ components/layout/Meta.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import React from "react";
+
+type Props = {};
+
+const Meta = (props: Props) => {
+  return (
+      <Head>
+        <title>Covid-19</title>
+      </Head>
+  );
+};
+
+export default Meta;
