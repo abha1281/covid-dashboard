7201948f64556fd0c8234a17f3d397df7e601eec
abha <abhaaibrahim@gmail.com>
2022-08-12 20:21:31 +0500

    Update overview.tsx

--- pages/overview.tsx
+++ pages/overview.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import NotificationMessage from "../components/global/molecules/NotificationMessage";
+import SidePanel from "../components/overview/SidePanel";
+import StatMap from "../components/overview/StatMap";
+import useChangeLayout from "../hooks/useChangeLayout";
 
-type Props = {}
+const Overview = () => {
+  const { handleChangeLayout } = useChangeLayout();
 
-const Overview = (props: Props) => {
+  useEffect(() => {
+    handleChangeLayout(true);
+  }, []);
+
   return (
-    <div>Overview</div>
-  )
-}
+    <div className="flex">
+      <div className="w-full">
+        <p className="font-black text-[27px] leading-[40.5px] text-[#261D56]">
+          Overview
+        </p>
+        <div className="mt-11 relative">
+          <div className="w-5 h-full absolute -left-1 inset-y-0 rounded-l-[20px] bg-[#FD79A8]" />
+          <NotificationMessage
+            title="Why social distancing is "
+            titleFocusText="important?"
+            imageName="OverviewIllustration"
+            widthOfImage={312}
+            heightOfImage={183}
+          />
+        </div>
+        <StatMap />
+      </div>
+      <SidePanel />
+    </div>
+  );
+};
 
-export default Overview
+export default Overview;
